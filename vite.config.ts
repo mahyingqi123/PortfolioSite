72c6b406b23e102428a1edf04cce0d2a5b4c155d
mahyingqi123 <ymah0009@student.monash.edu>
2025-02-02 16:38:03 +0800

    Update deloying config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/PortfolioSite/',  // Replace with your repo name
+  plugins: [react()]
 })
